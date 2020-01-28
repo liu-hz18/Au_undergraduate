1f6af37b4f0c3058df6176c551a26cb336872614
liu-hz18 <44592533+liu-hz18@users.noreply.github.com>
2020-01-28 12:35:53 +0800

    Update TSP_branchBound.cpp

--- 离散数学(2)/TSP/TSP_branchBound.cpp
+++ 离散数学(2)/TSP/TSP_branchBound.cpp
@@ -1,31 +1,31 @@
 
 #include "TSP.h"
 
-//ÓÃÓÚqsortµÄcompare
+//ç”¨äºqsortçš„compare
 int compare(const void* x, const void* y){
     return ((Edge*)x)->weight - ((Edge*)y)->weight;
 }
 
-//·ÖÖ§¶¨½ç·¨,dfs²ßÂÔ¼Ó±ßÅĞ¶Ï»ØÂ·
+//åˆ†æ”¯å®šç•Œæ³•,dfsç­–ç•¥åŠ è¾¹åˆ¤æ–­å›è·¯
 bool comGraph::findCircle(int edgenum, int rank, int sum){
-	//»Ö¸´Ô­À´×´Ì¬
+	//æ¢å¤åŸæ¥çŠ¶æ€
 	sum += edge[rank].weight;
-	if(sum > mincost)return false;//¼ôÖ¦
+	if(sum > mincost)return false;//å‰ªæ
 	count[edge[rank].from] ++;
 	count[edge[rank].to] ++;
-	//ÍËÕ»
-	if(count[edge[rank].from] > 2 || count[edge[rank].to] > 2)return false;//Ã»ÓĞ»ØÂ·
-	//¼Ó±ßÈëÕ»
+	//é€€æ ˆ
+	if(count[edge[rank].from] > 2 || count[edge[rank].to] > 2)return false;//æ²¡æœ‰å›è·¯
+	//åŠ è¾¹å…¥æ ˆ
 	if(edgenum+1 < n){
-		for(int i = rank+1; i <= upperVertex; i++){//upperVertexÏŞÖÆ»ØÂ·±ßµÄ×î´óÉÏ½ç£¬¼ôÖ¦
-			//ÓÉµİÔöĞÔÖÊ£¬¿ÉÒÔ¼ôÖ¦
+		for(int i = rank+1; i <= upperVertex; i++){//upperVertexé™åˆ¶å›è·¯è¾¹çš„æœ€å¤§ä¸Šç•Œï¼Œå‰ªæ
+			//ç”±é€’å¢æ€§è´¨ï¼Œå¯ä»¥å‰ªæ
 		bool flag = findCircle(edgenum+1, i, sum);
-			count[edge[i].from] --;//ÍË»ØÔ­ÓĞ×´Ì¬
+			count[edge[i].from] --;//é€€å›åŸæœ‰çŠ¶æ€
 			count[edge[i].to] --;
 			if(flag)return false;
 		}
 	}
-	//edgenum == n,ÕÒµ½ÁËÒ»¸ö»ØÂ·
+	//edgenum == n,æ‰¾åˆ°äº†ä¸€ä¸ªå›è·¯
 	if(edgenum+1 == n){
 		upperVertex = min(upperVertex, rank);
 		mincost = min(mincost, sum);
@@ -34,13 +34,13 @@ bool comGraph::findCircle(int edgenum, int rank, int sum){
 	return false;
 }
 
-//ÂÃĞĞÉÌ·ÖÖ§¶¨½ç·¨
+//æ—…è¡Œå•†åˆ†æ”¯å®šç•Œæ³•,æœ€åO(n!),ä½†ä¸€èˆ¬è¿œå°äºè¯¥å¤æ‚åº¦
 int comGraph::TSPBranchBound(){
-	assert(e == (n*(n-1))>>1);//ÍêÈ«Í¼
+	assert(e == (n*(n-1))>>1);//å®Œå…¨å›¾
 	mincost = INT_MAX;
 	upperVertex = e - 1;
 	qsort(edge, e, sizeof(Edge), compare);
-	//»ØÂ·ÓĞ¿ÉÄÜ²»±éÀúÃ¿Ò»Ìõ±ß£¬ËùÒÔÒª¶ÔÃ¿Ò»Ìõ±ß½øĞĞµİ¹é
+	//å›è·¯æœ‰å¯èƒ½ä¸éå†æ¯ä¸€æ¡è¾¹ï¼Œæ‰€ä»¥è¦å¯¹æ¯ä¸€æ¡è¾¹è¿›è¡Œé€’å½’
 	for(int i = 0; i <= e - n; i++){
 		memset(count, 0, n * sizeof(int));
 		findCircle(0, i, 0);
