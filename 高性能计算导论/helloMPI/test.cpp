3da634cc7860feb05f332a6f795c3c77e4b1f663
liu-hz18 <44592533+liu-hz18@users.noreply.github.com>
2020-02-09 12:18:12 +0800

    Add files via upload

---
+++ 高性能计算导论/helloMPI/test.cpp
@@ -0,0 +1,21 @@
+
+#include "mpi.h"   
+#include<stdio.h>
+
+int main(int argc, char* argv[]){  
+	int myid, numprocs;  
+	int namelen;   
+  
+	char processor_name[MPI_MAX_PROCESSOR_NAME];  
+ 
+	MPI_Init(&argc, &argv);  
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);  
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);  
+	MPI_Get_processor_name(processor_name, &namelen);  
+  
+	printf("Hello World! Process %d of %d on %s\n", myid, numprocs, processor_name);  
+  
+	MPI_Finalize(); 
+	return 0;  
+
+}
