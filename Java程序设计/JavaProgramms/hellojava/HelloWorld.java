4fe88d16de9a7474ce8ad1e56abf6c9851230deb
liu-hz18 <44592533+liu-hz18@users.noreply.github.com>
2020-03-02 11:16:36 +0800

    Add files via upload

---
+++ Java程序设计/JavaProgramms/hellojava/HelloWorld.java
@@ -0,0 +1,8 @@
+// HelloWorld.java
+package helloworld;
+
+public class HelloWorld {
+    public void say(){
+        System.out.println("Hello World");
+    }
+}
---
+++ Java程序设计/JavaProgramms/hellojava/Test.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class Test {
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.say();
+		System.out.println("You should say: Hello World");
+	}
+}
